506faef79668af885b199945b5eeba679c222e2a
hanull <dkfghg3@naver.com>
2021-02-02 09:34:00 +0900

    solve: boj 카드2 이한울 풀이 완료

---
+++ mon2/day01/boj_카드2/boj_카드2_이한울.java
@@ -0,0 +1,22 @@
+package day01.boj_카드2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class boj_카드2_이한울 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.add(i);
+        }
+        while (q.size() > 1) {
+            q.poll();
+            q.add(q.poll());
+        }
+        System.out.println(q.poll());
+    }
+}
