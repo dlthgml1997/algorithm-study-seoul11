8e995034c92a242f35b1a055a5425a408b12c881
kimminkwon <kmk130519@gmail.com>
2021-02-03 19:30:03 +0900

    solve: boj 0129problems 풀이완료

---
+++ mon1/day29/boj_ATM/boj_ATM_김민권.java
@@ -0,0 +1,33 @@
+package mon1.day29.boj_ATM;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class boj_ATM_김민권 {
+
+	private static int n;
+	private static List<Integer> timeList = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		makeInput();
+		findMinimumTimesAndPrint();
+	}
+
+	private static void findMinimumTimesAndPrint() {
+		System.out.println(timeList.stream().sorted().map(i -> i * n--).reduce(0, Integer::sum));
+	}
+
+	private static void makeInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		Arrays.stream(br.readLine().split(" ")).forEach(s -> timeList.add(Integer.parseInt(s)));
+	}
+}
---
+++ mon1/day29/boj_체스판다시칠하기/boj_체스판다시칠하기_김민권.java
@@ -0,0 +1,62 @@
+package mon1.day29.boj_체스판다시칠하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class boj_체스판다시칠하기_김민권 {
+
+	private static int N, M, result;
+	private static char[][] board;
+	private static char[][] trueBoardOne = {{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}};
+	private static char[][] trueBoardTwo = {{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+											{'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+											{'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}};
+	
+	public static void main(String[] args) throws IOException {
+		makeInput();
+		findMinimumDiffToCurrBoard();
+		printResult();
+	}
+
+	private static void findMinimumDiffToCurrBoard() {
+		for(int i = 0; i <= N-8; i++)
+			for(int j = 0; j <= M-8; j++)
+				result = Math.min(result, calculateDiffFromStart(i, j));
+	}
+
+	private static int calculateDiffFromStart(int iStart, int jStart) {
+		int diffToBoardOne = 0, diffToBoardTwo = 0;
+		for(int i = 0; i < 8; i++)
+			for(int j = 0; j < 8; j++) {
+				if(trueBoardOne[i][j] != board[i + iStart][j + jStart]) diffToBoardOne++;
+				if(trueBoardTwo[i][j] != board[i + iStart][j + jStart]) diffToBoardTwo++;
+			}
+		return Math.min(diffToBoardOne, diffToBoardTwo);
+	}
+
+	private static void makeInput() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken()); result = Integer.MAX_VALUE;
+		board = new char[N][M];
+		for(int i = 0; i < N; i++) board[i] = br.readLine().toCharArray();
+	}
+
+	private static void printResult() { System.out.println(result); }
+
+}
