69d596deadb8cc7aecd75a8e34d5cf4e2c39774b
kimminkwon <kmk130519@gmail.com>
2021-02-08 23:17:24 +0900

    solve: boj 피보나치수4 김민권

---
+++ mon2/day08/boj_피보나치수4/boj_피보나치수4_김민권.java
@@ -0,0 +1,24 @@
+package mon2.day08.boj_피보나치수4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class boj_피보나치수4_김민권 {
+
+    private static BigInteger[] memo;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        memo = new BigInteger[N + 1];
+        memo[0] = new BigInteger("0");
+        if(N > 0) memo[1] = new BigInteger("1");
+        System.out.println(fibonachi(N));
+    }
+
+    private static BigInteger fibonachi(int n) {
+        if(memo[n] == null) memo[n] = fibonachi(n - 1).add(fibonachi(n - 2));
+        return memo[n];
+    }
+}
