a88188c9ee46cbd875ad224f32088464878c3bc2
hanull <dkfghg3@naver.com>
2021-02-09 02:26:10 +0900

    sovle: 0204 알고 문제 완료

---
+++ mon2/day04/boj_연속합/boj_연속합_이한울.java
@@ -0,0 +1,31 @@
+package day04.boj_연속합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_연속합_이한울 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = stoi(br.readLine());
+        int[] dp = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            dp[i] = stoi(st.nextToken());
+        }
+        int max = dp[0];
+        for (int i = 1; i < N; i++) {
+            dp[i] = Math.max(dp[i], dp[i - 1] + dp[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+
+    }
+
+    static int stoi(String input) {
+        return Integer.parseInt(input);
+    }
+
+}
---
+++ mon2/day04/boj_피보나치함수/boj_피보나치함수_이한울.java
@@ -0,0 +1,33 @@
+package day04.boj_피보나치함수;
+
+import java.util.Scanner;
+
+public class boj_피보나치함수_이한울 {
+
+    static int[][] dp = new int[41][41];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        init();
+        while (T-- > 0) {
+            int N = sc.nextInt();
+            System.out.println(dp[N][0] + " " + dp[N][1]);
+        }
+    }
+
+    static void init() {
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        dp[2][0] = 1;
+        dp[2][1] = 1;
+
+        for (int i = 3; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+    }
+
+}
